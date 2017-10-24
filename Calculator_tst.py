b9aa4fbee5f6b7fb26f70f6faa996cc36e1ba549
Chris Redekop <chris.redekop@gmail.com>
2017-10-24 10:31:36 -0400

    Add operand test

--- Calculator_tst.py
+++ Calculator_tst.py
@@ -5,6 +5,7 @@ class MyTestCase(unittest.TestCase):
   def test_default_sum_set(self):
     hw = Calculator()
     self.assertEqual(hw.sum, 10)
+    self.assertEqual(hw.x, 6)
 
 if __name__ == '__main__':
   unittest.main()
